5acc4ba59c4974e59d52caa4d06d03e32863e8d0
pedrohote <pedro_hote@hotmail.com>
2023-05-18 12:32:56 -0300

    feat: Criado models para serviço de produção de material; Criada classe para registrar equipamentos, e para pedidos de material

--- SolicitacoesApp/admin.py
+++ SolicitacoesApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import EquipamentoProducaoDeMaterial, ProducaoDeMaterial, ServicoProducaoDeMaterial
 
 # Register your models here.
+admin.site.register(EquipamentoProducaoDeMaterial)
+admin.site.register(ServicoProducaoDeMaterial)
+admin.site.register(ProducaoDeMaterial)
+
+
--- SolicitacoesApp/models.py
+++ SolicitacoesApp/models.py
@@ -1,3 +1,44 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class ServicoProducaoDeMaterial(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class EquipamentoProducaoDeMaterial(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class ProducaoDeMaterial(models.Model):
+   
+    CHOICES_PARTICIPANTES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5')
+    )
+
+    professor_responsavel = models.CharField(max_length=100)
+    servicos = models.ManyToManyField(ServicoProducaoDeMaterial)
+    dia_agendamento = models.DateField()
+    horario_agendamento = models.TimeField()
+    duracao_gravacao = models.CharField(max_length=20)
+    data_entrega_material = models.DateField()
+    arte_pronta = models.FileField(upload_to='arte_pronta/', null=True, blank=True)
+    criar_arte = models.BooleanField(default=False)
+    setor_curso = models.CharField(max_length=100)
+    projeto_disciplina = models.TextField()
+    equipamentos = models.ManyToManyField(EquipamentoProducaoDeMaterial)
+    equipe_cead = models.BooleanField(default=True)
+    numero_participantes = models.IntegerField(choices=CHOICES_PARTICIPANTES)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    observacao = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Produção de Material - {self.professor_responsavel}"
