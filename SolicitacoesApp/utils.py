aa58d4d5f42080a72cc0dfea7d5687e8c0761a78
pedrohote <pedro_hote@hotmail.com>
2023-05-22 15:43:43 -0300

    feat: Função para envio de email

--- SolicitacoesApp/utils.py
+++ SolicitacoesApp/utils.py
@@ -2,26 +2,23 @@ from django.conf import settings
 from django.core.mail import send_mail
 from typing import List
 
-class SendEmail(object):
-    """Função responsável pelo envio de emails
+def message_producao(data: dict) -> str:
+    message = f'''
+        Professor Responsável: {data['professor_responsavel']}
+        Finalidade da Gravação: {data['finalidade_gravacao']}
+        Horário de Agendamento: {data['horario_agendamento']}
+        Duração da Gravação: {data['duracao_gravacao']}
+        Data de Entrega do Material: {data['data_entrega_material']}
+        Serviços: {', '.join(data['servicos']) if 'servicos' in data else ''} 
+        Arte para produção de material: {data['arte_pronta'] if 'arte_pronta' in data else ''}  # Adiciona o valor do campo 'arte_pronta', se existir
+        Precisa criar arte? {data['criar_arte']}
+        Setor Curso: {data['setor_curso']}
+        Equipamentos: {', '.join(data['equipamentos']) if 'equipamentos' in data else ''}  # Adiciona os valores dos campos relacionados a 'equipamentos', se existirem
+        Precisará de nossa equipe de cinegrafistas? {data['equipe_cead']}
+        Número de participantes: {data['numero_participantes']}
+        E-mail: {data['email']}
+        Telefone: {data['telefone']}
+        Observações: {data['observacao']}         
+    '''
 
-    Args:
-        subject: string
-        message: string
-        email_from: string
-        recipients: list of strings
-    """
-    def __init__(
-        self, 
-        subject: str = None, 
-        message: str = None, 
-        email_from: str = settings.EMAIL_HOST_USER, 
-        recipients: List[str] = None
-    ) -> None:
-        self.subject = subject
-        self.message = message
-        self.emailfrom = email_from
-        self.recipients = recipients
-
-    def send(self):
-        return send_mail(self.subject, self.message, self.emailfrom, self.recipients)
+    return message
--- SolicitacoesApp/views.py
+++ SolicitacoesApp/views.py
@@ -4,6 +4,13 @@ from .models import *
 from django.urls import reverse_lazy
 from django import forms
 
+# Importe a função message_producao do arquivo utils.py
+from SolicitacoesApp.utils import message_producao
+
+# Importe os módulos necessários do Django
+from django.conf import settings
+from django.core.mail import send_mail
+
 # Create your views here.
 def Index(request):
 
@@ -21,3 +28,20 @@ class ProducaoDeMaterialCreateView(CreateView) :
         form.fields['horario_agendamento'].widget = forms.TimeInput(attrs={'type': 'time'})
         form.fields['data_entrega_material'].widget = forms.DateInput(attrs={'type': 'date'})
         return form
+        
+    # Defina o método form_valid na sua classe CreateView
+    def form_valid(self, form):
+        """
+        Caso o formulário seja válido, envie um e-mail com os dados do formulário para o responsável
+        """
+        send_mail(
+            subject=f"Solicitação - {form.cleaned_data['professor_responsavel']}",  # Defina o assunto do e-mail
+            message=message_producao(form.cleaned_data),  # Defina o corpo do e-mail usando a função message_producao
+            from_email=settings.EMAIL_HOST_USER,  # Defina o remetente do e-mail 
+            recipient_list=['seu@email.com']  # Defina os destinatários do e-mail
+        )
+        
+        # Continue com o comportamento padrão do form_valid
+        return super().form_valid(form)
+
+
