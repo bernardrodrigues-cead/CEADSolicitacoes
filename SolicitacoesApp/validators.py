68ff80a6d723a5989a305512888144ace6e66891
bernardrodrigues-cead <bernard.rodrigues@uab.ufjf.br>
2023-05-22 09:31:34 -0300

    Adicionada docstring aos validadores

--- SolicitacoesApp/validators.py
+++ SolicitacoesApp/validators.py
@@ -1,12 +1,28 @@
 from django.core.exceptions import ValidationError
 
-def validate_professor_responsavel(value): # Verifica se a string contém dois ou mais palavras com dois ou mais caracteres 
+def validate_professor_responsavel(value):
+    """Verifica se a string contém dois ou mais palavras com dois ou mais caracteres 
+
+    Args:
+        value (string): string a ser validada
+
+    Raises:
+        ValidationError: Erro de validação
+    """
     substrings = value.split() # Quebra a string em substrings (partes da cadeia de caracteres divididas por espaço)
     valid_substrings = sum(1 for substring in substrings if len(substring) >= 2) # adiciona +1 a valid_substrings para cada substring de tamanho >= 2
     if valid_substrings < 2: # Se a quantidade de substrings válidas forem menores que 2,
         raise ValidationError('Nome inválido (Preencha o nome completo).') # Chama o erro
 
-def validate_finalidade_gravacao(value): # Verifica se a string contém dois ou mais caracteres
+def validate_finalidade_gravacao(value):
+    """Verifica se a string contém dois ou mais caracteres
+
+    Args:
+        value (string): string a ser validada
+
+    Raises:
+        ValidationError: Erro de validação
+    """
     substrings = value.split()  # Quebra a string em substrings (partes da cadeia de caracteres divididas por espaço)
     valid_substrings = sum(1 for substring in substrings if len(substring) >= 2) # adiciona +1 a valid_substrings para cada substring de tamanho >= 2
     if valid_substrings < 2 or len(value) < 30:  # Se a quantidade de substrings válidas forem menores que 2, ou a quantidade de caracteres for maior que 30
