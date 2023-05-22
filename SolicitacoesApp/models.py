d54f2b9ab92dcde7a8d337e3be26c8636561c805
bernardrodrigues-cead <bernard.rodrigues@uab.ufjf.br>
2023-05-22 15:47:09 -0300

    fix: ajuste de models e validators

--- SolicitacoesApp/models.py
+++ SolicitacoesApp/models.py
@@ -1,6 +1,6 @@
 # Create your models here.
 from django.db import models
-from .validators import validate_professor_responsavel, validate_finalidade_gravacao
+from .validators import validate_professor_responsavel, validate_min_30
 
 
 class ServicoProducaoDeMaterial(models.Model):
@@ -24,26 +24,24 @@ class ProducaoDeMaterial(models.Model):
         (4, '4'),
     )
     
-    finalidade_gravacao = models.TextField(verbose_name='Finalidade da Gravação', validators=[validate_finalidade_gravacao])
-
     professor_responsavel = models.CharField(max_length=100, verbose_name='Professor Responsável', validators=[validate_professor_responsavel])
-    servicos = models.ManyToManyField('ServicoProducaoDeMaterial', blank=True, verbose_name='Serviços')
-    horario_agendamento = models.TimeField(verbose_name='Horário de Agendamento')
-    duracao_gravacao = models.CharField(max_length=20, verbose_name='Duração da Gravação')
-    data_entrega_material = models.DateField(verbose_name='Data de Entrega do Material')
-    arte_pronta = models.FileField(upload_to='arte_pronta/', null=True, blank=True, verbose_name='Arte para produção de material')
-    criar_arte = models.BooleanField(default=False, verbose_name='Precisa criar arte?')
     setor_curso = models.CharField(max_length=100, verbose_name="Setor Curso")
-   
- 
+    email = models.EmailField(verbose_name="E-mail")
+    telefone = models.CharField(max_length=20)
+    data_agendamento = models.DateField(verbose_name='Data de Agendamento')
+    horario_agendamento = models.TimeField(verbose_name='Horário de Agendamento')
+    duracao_estimada = models.CharField(max_length=20, verbose_name='Duração da Estimada')
+    data_entrega_material = models.DateField(verbose_name='Data de Entrega do Material')
+    servicos = models.ManyToManyField('ServicoProducaoDeMaterial', blank=True, verbose_name='Serviços')
+    finalidade_gravacao = models.TextField(verbose_name='Finalidade da Gravação', validators=[validate_min_30])
+    arte_pronta = models.FileField(upload_to='arte_pronta/', null=True, blank=True, verbose_name='Arte para produção de material')
+    detalhes_arte = models.TextField(verbose_name='Descreva sua arte aqui', blank=True, validators=[validate_min_30])
     equipamentos = models.ManyToManyField('EquipamentoProducaoDeMaterial', blank=True)
     equipe_cead = models.BooleanField(verbose_name='Precisará de nossa equipe de cinegrafistas?')
     numero_participantes = models.IntegerField(choices=CHOICES_PARTICIPANTES, verbose_name='Número de participantes')
-    email = models.EmailField(verbose_name="E-mail")
-    telefone = models.CharField(max_length=20)
     observacao = models.CharField(max_length=255, verbose_name='Observações', blank=True)
+
     criado_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Produção de Material-{self.professor_responsavel}"
-
