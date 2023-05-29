7cd3840b671301e7f712bc2bf250835bbd34226d
bernardrodrigues-cead <bernard.rodrigues@uab.ufjf.br>
2023-05-29 17:38:27 -0300

    Armazenamento de arquivos e envio de email customizado

--- SolicitacoesApp/utils.py
+++ SolicitacoesApp/utils.py
@@ -9,6 +9,7 @@ CARD_CONTENT = [
 ]
 
 CHOICES_PARTICIPANTES = (
+    (0, '0'),
     (1, '1'),
     (2, '2'),
     (3, '3'),
@@ -20,23 +21,4 @@ CHOICES_EQUIPE_CEAD = (
     (True, 'Sim, precisaremos da equipe do CEAD')
 )
 
-def message_producao(data: dict) -> str:
-        # Serviços: {', '.join(data['servicos']) if 'servicos' in data else ''} 
-        # Equipamentos: {', '.join(data['equipamentos']) if 'equipamentos' in data else ''}  
-    message = f'''
-        Professor Responsável: {data['professor_responsavel']}
-        Finalidade da Gravação: {data['finalidade_solicitacao']}
-        Horário de Agendamento: {data['horario_agendamento']}
-        Duração da Gravação: {data['duracao_estimada']}
-        Data de Entrega do Material: {data['data_entrega_material']}
-        Arte para produção de material: {data['arte_pronta'] if 'arte_pronta' in data else ''}
-        Detalhes da arte: {data['detalhes_arte']}
-        Setor Curso: {data['setor_curso']}
-        Precisará de nossa equipe de cinegrafistas? {data['equipe_cead']}
-        Número de participantes: {data['numero_participantes']}
-        E-mail: {data['email']}
-        Telefone: {data['telefone']}
-        Observações: {data['observacao']}      
-    '''
-
-    return message
+EMAIL_PRODUCAO = 'bernard.rodrigues@uab.ufjf.br'
