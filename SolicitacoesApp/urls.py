0d28e2ae7ddcbbfc2e0771b9d69d5d756b8efa9d
pedrohote <pedro_hote@hotmail.com>
2023-06-20 16:50:42 -0300

    criando view para o model de viagem

--- SolicitacoesApp/urls.py
+++ SolicitacoesApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.Index, name='index'),
     path('producao/new/', views.ProducaoDeMaterialCreateView.as_view(), name="producao_create"),
+    path('viagem/new/', views.ViagemCreateView.as_view(), name='viagem_create'),
 ]
